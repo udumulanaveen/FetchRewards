98c1bd1f4f8e0c018cfc67da27e0d4a3a40872b5
naveenudumula <udumv01@pfw.edu>
2024-10-24 12:26:14 -0500

    Removed duplicates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,9 +76,7 @@ dependencies {
     implementation ("androidx.compose.material:material:1.3.0")
     implementation ("androidx.compose.ui:ui-tooling-preview:1.3.0")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
 
 
 
